5cdc86ca789e761da64cd1dc3f1cfec8cff897b7
David MENTRÉ <d.mentre@fr.merce.mee.com>
2024-02-12 10:53:35 +0100

    Remove absolute path in ./scripts/coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,6 +2,6 @@ dune clean
 rm -Rf _coverage
 rm *.coverage
 BISECT_ENABLE=yes dune build
-./_build/default/req2something.exe --input-dir /home/cellier/req2something/reqs 
+./_build/default/req2something.exe --input-dir ./reqs
 bisect-ppx-report html
 echo "Open _coverage/index.html file with your favorite web browser to see coverage"
