70b788106810f8983eb315e81d91a185aab02a83
Ocan Sankur <o.sankur@fr.merce.mee.com>
2024-12-18 13:56:27 +0100

    updated install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,10 @@
 
 if ! command -v opam &> /dev/null
 then
-  echo "Please install opam using your package manager"
-  exit 1
+  if ! command -v sudo apt install opam
+    echo "Please install opam using your package manager"
+    exit 1
+  fi
 fi
 if ! command -v pip3 &> /dev/null
 then
@@ -11,12 +13,15 @@ then
   exit 1
 fi
 
-
+set -e
 # ppx inline test does not install properly with opam when using ocaml 5.2.0
 # ocaml 5.1.0 is needed 
-opam switch create ocaml.5.1.0
+opam switch -y create ocaml.5.1.0
+eval $(opam env)
 opam install -y dune menhir alcotest ppx_inline_test bisect_ppx ppx_expect
 cd ../lib/translator
-dune build
 opam install .
-pip3 install timeout-decorators z3-solver
+pip3 install timeout-decorator z3-solver
+./setup_nusmv.sh
+./setup_pono.sh
+./setup_spot.sh
--- scripts/setup_spot.sh
+++ scripts/setup_spot.sh
@@ -6,4 +6,12 @@ cd spot-2.12.1
 ./configure --prefix ~/local/
 make
 make install
+export PATH=$PATH:~/local/bin/
 echo "export PATH=$PATH:~/local/bin/" >> ~/.bashrc
+source ~/.bashrc
+# sudo su
+# wget -q -O - https://www.lrde.epita.fr/repo/debian.gpg | apt-key add -
+# echo 'deb http://www.lrde.epita.fr/repo/debian/ stable/' >> /etc/apt/sources.list
+# exit
+# sudo apt-get update
+# sudo apt-get install spot libspot-dev spot-doc python3-spot
